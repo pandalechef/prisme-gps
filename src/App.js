b2bfbfabc4c8338be586278b0a3a49e939abd674
Julien Seng <julien.seng@insee.fr>
2018-09-24 10:39:33 +0200

    ajout casdetest pas de sim

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Geolocalisation from './components/geolocalisation';
-import Export from './components/export';
-import Parametres from './components/parametres';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Geolocalisation from "./components/geolocalisation";
+import Export from "./components/export";
+import Parametres from "./components/parametres";
 function TabContainer(props) {
   return (
     <Typography component="div" style={{ padding: 8 * 3 }}>
@@ -31,8 +31,8 @@ class SimpleTabs extends React.Component {
   state = {
     value: 0,
     releves: [],
-    casDeTest: ['dehors', 'bar'],
-    reseau: ['SIM voix+data', 'SIM data']
+    casDeTest: ["dehors", "bar"],
+    reseau: ["SIM voix+data", "SIM data", "pas de SIM"]
   };
 
   handleChange = (event, value) => {
@@ -49,11 +49,11 @@ class SimpleTabs extends React.Component {
     });
   };
 
-  majErreurMesureDernierReleve = (erreurMesure) =>{
+  majErreurMesureDernierReleve = erreurMesure => {
     var releves = [...this.state.releves];
-    releves[releves.length - 1].erreurMesure = erreurMesure
-    this.setState({releves})
-  }
+    releves[releves.length - 1].erreurMesure = erreurMesure;
+    this.setState({ releves });
+  };
 
   ajouterCasDeTest = nouveauCas => {
     this.setState({ casDeTest: [...this.state.casDeTest, nouveauCas] });
@@ -75,7 +75,7 @@ class SimpleTabs extends React.Component {
             <Tab label="Paramètres" />
           </Tabs>
         </AppBar>
-        {value === 0 &&
+        {value === 0 && (
           <TabContainer>
             <Geolocalisation
               casDeTest={this.state.casDeTest}
@@ -83,18 +83,21 @@ class SimpleTabs extends React.Component {
               ajoutReleve={this.ajoutReleve}
               majErreurMesureDernierReleve={this.majErreurMesureDernierReleve}
             />
-          </TabContainer>}
-        {value === 1 &&
+          </TabContainer>
+        )}
+        {value === 1 && (
           <TabContainer>
             <Export releves={this.state.releves} />
-          </TabContainer>}
-        {value === 2 &&
+          </TabContainer>
+        )}
+        {value === 2 && (
           <TabContainer>
             <Parametres
               ajouterCasDeTest={this.ajouterCasDeTest}
               ajouterReseau={this.ajouterReseau}
             />
-          </TabContainer>}
+          </TabContainer>
+        )}
       </div>
     );
   }
